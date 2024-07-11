25075ff0973f772b45c1ddb511019754abbfc53f
birju0451 <165183340+birju0451@users.noreply.github.com>
2024-07-11 18:33:15 +0530

    problem solving

---
+++ JAVA PROGRAMMING/LOOPS/cuntNunber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class cuntNunber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int n=sc.nextInt();
+        int count=0;
+        while(n!=0){
+            n=n/10;
+            count ++;
+        }
+        System.out.println("total number present in n"+count);
+        sc.close();
+
+    }
+    
+}

3e4c8c18b2d2a7b7a9e62cc7a288e4172ba07760
birju0451 <165183340+birju0451@users.noreply.github.com>
2024-07-12 20:59:12 +0530

    first commit

--- JAVA PROGRAMMING/3.LOOPS/sumOfdigits.java
+++ JAVA PROGRAMMING/3.LOOPS/sumOfdigits.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class cuntNunber {
+public class sumOfdigits {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number:");
+        System.out.println("Enter a number:");
         int n=sc.nextInt();
-        int count=0;
+        int sum=0;
         while(n!=0){
+            int rem =n%10;
+            sum =sum+rem;
             n=n/10;
-            count ++;
+            
         }
-        System.out.println("total number present in n"+count);
+        System.out.println("sum of digits:"+sum);
         sc.close();
 
     }
-    
 }
--- JAVA PROGRAMMING/3.LOOPS/sumofN.java
+++ JAVA PROGRAMMING/3.LOOPS/sumofN.java
@@ -5,7 +5,7 @@ public class sumofN {
         Scanner sc =new Scanner(System.in);
         System.out.println("enter the number:");
         int n=sc.nextInt();
-        int sum=0,i=1;
+        int sum=0,i=0;
         while(i<=n){
             sum=sum+i;
             i++;

1e191d4f8b06985737f33945f6a200213d1a79a8
birju0451 <birjukumar1252005@gmail.com>
2024-07-15 23:05:32 +0530

    initial commit

---
+++ JAVA PROGRAMMING/4.OPPS/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        student obj1=new student();
+        System.out.println(obj1.rollNo);
+        System.out.println(obj1.studentName);
+
+    }
+}

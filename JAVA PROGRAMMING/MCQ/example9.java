6263782e40e4e3ac69753f27bbb05a5bdf4769c0
birju0451 <birjukumar1252005@gmail.com>
2024-07-18 19:23:00 +0530

    initial comment for opps

---
+++ JAVA PROGRAMMING/MCQ/example9.java
@@ -0,0 +1,7 @@
+public class example9 {
+    public static void main(String[] args) {
+        String str1="hello";
+        String str2=new String("hello");
+        System.out.println(str1.equals(str2));
+    }
+}

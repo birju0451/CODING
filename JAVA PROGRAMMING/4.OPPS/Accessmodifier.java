b223ed714e36aae060ee6be16ad2778272fc7f67
birju0451 <birjukumar1252005@gmail.com>
2024-07-19 22:37:15 +0530

    initial

--- JAVA PROGRAMMING/4.OPPS/Accessmodifier.java
+++ JAVA PROGRAMMING/4.OPPS/Accessmodifier.java
@@ -1,10 +1,17 @@
-public class Accessmodifier {
-    public String str_1="i am a public member";
-    void printfromclass(){
-        System.out.println("within class");
-    }
-    public static void main(String[] args) {
-        Accessmodifier obj=new Accessmodifier();
-        System.out.println(obj.str_1);
-    }
-}
+// public class Accessmodifier {
+//     public String str_1="i am a public member";
+//     void printfromclass(){
+//         System.out.println("within class"+" " +str_1);
+//     }
+//     public static void main(String[] args) {
+//         Accessmodifier obj=new Accessmodifier();
+//         obj.printfromclass();
+//         System.out.println("within class"+obj.str_1);
+//     }
+// }
+// class Accessmodifier1{
+//     void printoutsideofClass(){
+//         Accessmodifier1 obj=new Accessmodifier1();
+//         System.out.println("within package ,outside claas");
+//     }
+// }
--- JAVA PROGRAMMING/4.OPPS/constructor.java
+++ JAVA PROGRAMMING/4.OPPS/constructor.java
@@ -1,39 +1,3 @@
-// import java.util.Scanner;
-
-// class Algebra{
-//     int a;
-//     int b;
-//     Algebra(int x,int y){//constructor to be checked
-//         System.out.println("constructure to be called");
-//         int a=x;
-//         int b=y;
-//     }
-//     int add(){
-//         int ans=a+b;
-//         return ans;
-//     }
-//     int sub(){
-//         return a-b;
-//     }
-//     int mul(){
-//         return a*b;
-//     }
-// }
-// public class constuctor {
-//     public static void main(String[] args) {
-//         Algebra obj=new Algebra(int x=5, int y=7);
-//         // Scanner sc =new Scanner(System.in);
-//         // System.out.println("enter the value of x:");
-//         // int x = sc.nextInt();
-//         // System.out.println("enter the value of y:");
-//         // int y = sc.nextInt();
-//         // int result=obj.add();
-//         //System.out.println("sum of x and y is :"+result);
-//         // sc.close();
-
-//     }
-    
-// }
 import java.util.Scanner;
 
 class Algebra {
@@ -59,15 +23,21 @@ class Algebra {
         return a * b;
     }
 }
-
 public class constructor {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         //@SuppressWarnings("unused")
+
         Algebra obj=new Algebra(5, 7);
         System.out.println(obj.add());
         System.out.println(obj.sub());
         System.out.println(obj.mul());
+
+        Algebra obj1=new Algebra(10, 4);
+        System.out.println(obj1.add());
+        System.out.println(obj1.sub());
+        System.out.println(obj1.mul());
+
         sc.close();
     }
 }

23afb1f43b9249f538142e1567891e3454c693a5
birju0451 <birjukumar1252005@gmail.com>
2024-09-06 18:28:37 +0530

    .

--- JAVA/5.ARRAY3/he.java
+++ JAVA/5.ARRAY3/he.java
@@ -2,5 +2,6 @@ public class he {
     public static void main(String[] args) {
         System.out.println("java");
         System.out.println("hellow world");
+        System.out.println("harsh");
     }
 }
